86a271e6fc1c9af8b2f53d071d1e69eea4e745bc
Ezz690 <abouelez10@gmail.com>
2024-01-29 18:54:26 +0200

    finish update method

--- TODO App/Back-end(api's)/todo_app(Laravel)/app/Http/Controllers/API/TaskController.php
+++ TODO App/Back-end(api's)/todo_app(Laravel)/app/Http/Controllers/API/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateTaskRequest;
 use App\Models\Task;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -29,9 +30,14 @@ class TaskController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * here i use form request for validation
      */
     public function store(CreateTaskRequest $request)
     {
+        /*
+        first check if user enter specific date for created task, if user doesn't enter any date the create task with today's date
+        */
         if (!$request->has('date')) {
             $request['date'] = Carbon::parse(Carbon::today())->format('Y-m-d');
         }
@@ -61,10 +67,27 @@ class TaskController extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
+     * here i type validation rules in function
      */
     public function update(Request $request, Task $task)
     {
-        //
+
+        if (!$this->check_authorization($task, Auth::user())) {
+            return response()->json(['message' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $request->validate([
+            'title' => 'string|min:3|max:100',
+            'date' => 'date|after_or_equal:today'
+        ]);
+
+        $task->update($request->all());
+
+        return response()->json([
+            'message' => 'Updated Successfully',
+            'task' => $task
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -72,6 +95,14 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        // $task->delete()
+    }
+
+    private function check_authorization(Task $task, User $user)
+    {
+        if ($user->id != $task->user_id)
+            return false;
+
+        return true;
     }
 }
